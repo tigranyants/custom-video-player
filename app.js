98db2368f5405aefdfb0a1b9754e67a00eb9f566
tigranyants <tigranyants99@gmail.com>
2022-06-24 21:29:02 +0500

    1

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 "use strict"
+
+let video = document.querySelector("#video");
+let playPause = document.querySelector("#playPause");
+
+let stop = document.querySelector("#stop");
+let mute = document.querySelector("#mute");
+
+let volup = document.querySelector("#volup");
+let voldown = document.querySelector("#voldown");
+let progress = document.querySelector("#progress")
+let progressBar = document.querySelector("#progress__bar")
+
+const startStop = ()=>{
+    let videoState = playPause.getAttribute("data-video-state")
+
+    if(videoState === "pause"){
+        video.play()
+        playPause.setAttribute("data-video-state", "play")
+    }
+    else{
+        video.pause()
+        playPause.setAttribute("data-video-state", "pause")
+    }
+};
+
+playPause.addEventListener("click", startStop)
+
+
+video.addEventListener("loadedmetadata",()=>{
+    progress.setAttribute("max",video.duration);
+})
+
+video.addEventListener("timeupdate", () =>{
+    progress.value = video.currentTime
+    progressBar.style.width = Math.floor((video.currentTime / video.duration * 100 + "%"))
+});
